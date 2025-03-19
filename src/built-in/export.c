0c325299aeeaf626d3fe843218227f25c9f10680
Arthur Legrix <alegrix@c2r7p3.42lehavre.fr>
2025-03-19 23:51:23 +0100

    try

--- src/built-in/export.c
+++ src/built-in/export.c
@@ -6,7 +6,7 @@
 /*   By: alegrix <alegrix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 23:46:53 by alegrix           #+#    #+#             */
-/*   Updated: 2025/03/13 23:50:54 by alegrix          ###   ########.fr       */
+/*   Updated: 2025/03/16 01:30:12 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 void	ft_export(t_env *env, char *line)
 {
 	t_env	*tmp;
+	t_env	*pre;
+	t_env	*nex;
+
+	int		i;
 
 	tmp = env;
+	i = 0;
+	while (line[i] && line[i] != ' ' && line[i] != '=')
+		i++;
 	while (tmp)
+	{
+		if (ft_strncmp(line, tmp->line, i) == 0)
 		tmp = tmp->next;
+	}
 	tmp = ft_envnew(line);
 }
