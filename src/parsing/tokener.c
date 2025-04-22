2e10705b838b3cbd2eaa5ed55030b1af0d43c20e
Achille BOSC <82178406+achille-bsc@users.noreply.github.com>
2025-04-22 18:54:42 +0200

    tokener starting

--- src/parsing/tokener.c
+++ src/parsing/tokener.c
@@ -6,66 +6,35 @@
 /*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 00:05:07 by abosc             #+#    #+#             */
-/*   Updated: 2025/04/02 22:45:34 by abosc            ###   ########.fr       */
+/*   Updated: 2025/04/22 18:53:09 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-t_exec	*tokener(char **parsed_prompt)
+t_list	*get_words(char *prompt)
 {
+	t_list	*words;
+	t_list	*init_word;
 	int		i;
-	int		j;
-	char	*arg;
-	t_exec	*exec;
-	t_args	*args;
+	int		in_dquote;
+	int		in_squote;
 
-	i = 0;
-	j = 0;
-	exec = ft_calloc(sizeof(t_exec), 1);
-	while (parsed_prompt[i])
+	words = ft_calloc(sizeof(t_list), sizeof(prompt));
+	init_word = words;
+	while (prompt[i])
 	{
-		arg = ft_calloc(sizeof(char), ft_strlen(parsed_prompt[i]));
-		while (parsed_prompt[i][j])
-		{
-			if (parsed_prompt[i][j] == '|' && parsed_prompt[i][j - 1] != '\\')
-			{
-
-				arg[j] = '\0';
-				args->name = ft_strdup(arg);
-				free(arg);
-				exec->args = args;
-				exec->next = ft_calloc(sizeof(t_exec), 1);
-				exec = exec->next;
-				args = ft_calloc(sizeof(t_args), 1);
-			}
-			else if (parsed_prompt[i][j] == '>')
-			{
-				exec->args = args;
-				exec->tok = e_token->TR;
-				exec->next = ft_calloc(sizeof(t_exec), 1);
-				exec = exec->next;
-				args = NULL;
-			}
-			else if (parsed_prompt[i][j] == '<')
-			{
-				
-				exec->args = args;
-				exec->tok = HD;
-				exec->next = ft_calloc(sizeof(t_exec), 1);
-				exec = exec->next;
-				args = NULL;
-			}
-			else if (parsed_prompt[i][j] == ' ')
-			{
-				args->next = ft_calloc(sizeof(t_args), 1);
-				args = args->next;
-			}
-			else
-			{
-				args->name = ft_strjoin(args->name, parsed_prompt[i][j]);
-			}
-		}
-		i++;
+		in_dquote = set_dquote(prompt[i], in_dquote);
+		in_squote = set_squote(prompt[i], in_squote);
+		if (!in_dquote &&  (prompt[i] == '<' || prompt[i] == '>'))
 	}
+	return (words);
+}
+
+t_list	*tokener(char *prompt)
+{
+	t_list	*words;
+
+	words = get_words(prompt);
+	return (words);
 }
