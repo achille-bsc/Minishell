3df08e98115ef8378ae6f80f1c78b46e584cc9d3
Achille BOSC <82178406+achille-bsc@users.noreply.github.com>
2025-05-27 00:44:04 +0200

    fix(parsing): :bug: correct quotes verif returning varlue for NULL

--- src/parsing/verifs.c
+++ src/parsing/verifs.c
@@ -6,7 +6,7 @@
 /*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 22:48:01 by abosc             #+#    #+#             */
-/*   Updated: 2025/05/27 00:18:03 by abosc            ###   ########.fr       */
+/*   Updated: 2025/05/27 00:43:15 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 void	verif(t_mnours *mnours)
 {
 	if (check_quotes(1, mnours->line) == 1)
-		return (ft_error("Syntax Error: incorrect quotes (single)", mnours),
-			mnours->exit = 1, 1);
+	{
+		ft_error("Syntax Error: incorrect quotes (single)", mnours);
+		mnours->exit = 1;
+	}
 	if (check_quotes(2, mnours->line) == 1)
-		return (ft_error("Syntax Error: incorrect quotes (double)", mnours),
-			mnours->exit = 1, 2);
-	return (0);
+	{
+		ft_error("Syntax Error: incorrect quotes (double)", mnours);
+		mnours->exit = 1;
+	}
+	return (NULL);
 }
 
 int	check_quotes(int type, char *line)
