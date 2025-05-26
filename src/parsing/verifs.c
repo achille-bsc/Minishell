27b9c76b6c8c2dbe5ae1da083f662742e54edac3
Achille BOSC <82178406+achille-bsc@users.noreply.github.com>
2025-05-27 00:47:03 +0200

    fix(parsing): :bug: correct

--- src/parsing/verifs.c
+++ src/parsing/verifs.c
@@ -6,26 +6,11 @@
 /*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 22:48:01 by abosc             #+#    #+#             */
-/*   Updated: 2025/05/27 00:43:15 by abosc            ###   ########.fr       */
+/*   Updated: 2025/05/27 00:46:41 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
-
-void	verif(t_mnours *mnours)
-{
-	if (check_quotes(1, mnours->line) == 1)
-	{
-		ft_error("Syntax Error: incorrect quotes (single)", mnours);
-		mnours->exit = 1;
-	}
-	if (check_quotes(2, mnours->line) == 1)
-	{
-		ft_error("Syntax Error: incorrect quotes (double)", mnours);
-		mnours->exit = 1;
-	}
-	return (NULL);
-}
 
 int	check_quotes(int type, char *line)
 {
@@ -47,4 +32,18 @@ int	check_quotes(int type, char *line)
 	if (counter % 2 == 1)
 		return (1);
 	return (0);
+}
+
+void	verif(t_mnours *mnours)
+{
+	if (check_quotes(1, mnours->line) == 1)
+	{
+		ft_error("Syntax Error: incorrect quotes (single)", mnours);
+		mnours->exit = 1;
+	}
+	if (check_quotes(2, mnours->line) == 1)
+	{
+		ft_error("Syntax Error: incorrect quotes (double)", mnours);
+		mnours->exit = 1;
+	}
 }
