75ff3e57e51e05292ad8f230557cfb742a14dceb
Arthur Legrix <alegrix@c2r8p3.42lehavre.fr>
2025-06-12 23:48:13 +0200

    fix leak ls |

--- src/parsing/tokener/tokener.c
+++ src/parsing/tokener/tokener.c
@@ -6,7 +6,7 @@
 /*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 00:05:07 by abosc             #+#    #+#             */
-/*   Updated: 2025/06/12 22:59:43 by abosc            ###   ########.fr       */
+/*   Updated: 2025/06/12 23:45:49 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,7 +108,7 @@ int	tokener(t_mnours *mnours, t_exec *exec, t_args *tokens)
 
 	words[1] = get_words(mnours->line);
 	if (verif_words(words[1]))
-		return (1);
+		return (ft_free_word(words[1]), 1);
 	words[0] = words[1];
 	while (words[1])
 	{
